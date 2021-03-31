2bb6736ab47a1fcae7a298c8c43faaebb401ec43
wheatmushi <9000ofhate@gmail.com>
2021-04-01 00:01:51 +0300

    test companies list added

---
+++ companies.py
@@ -0,0 +1,8 @@
+# A-companies: revenue, income > 0
+A_list = ('msft', 'aapl', 'nrg', 'jnj', 'v', 'dis', 't', 'f', 'csco', 'xom', 'ko', 'mcd', 'wfc', 'ba', 'c', 'bmy',
+          'nee', 'gs', 'rio', 'amd', 'intc', 'lmt', 'gm', 'gild', 'vale', 'duk', 'atvi', 'cop', 'vmw', 'dell', 'abbv',
+          'vrtx', 'fb', 'khc', 'vz', 'dg', 'wba', 'ebay', 'biib', 'bk', 'spg', 'luv', 'gold', 'afl', 'slf', 'alxn',
+          'mpc', 'jpm', 'wmt', 'pg', 'nvda', 'adbe', 'cvx', 'pfe', 'hd', 'nflx', 'orcl', 'nvs', 'txn', 'lly', 'tmo',
+          'mrk', 'ups', 'pm', 'qcom', 'ms', 'jd', 'tot', 'ibm', 'amat', 'cat', 'el', 'antm', 'tgt', 'intu', 'cni',
+          'fisv', 'isrg', 'pld', 'bns', 'usb', 'mdlz', 'ci', 'spgi', 'cci', 'shw', 'bdx', 'csx', 'fdx', 'dell', 'hca',
+          'adsk', 'd', 'ice', 'abb', 'gpn', 'lfc', 'ilmn', 'wm', 'bsx', 'tak', 'adi')
